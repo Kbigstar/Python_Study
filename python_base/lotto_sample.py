47dcc713c0fef75dcadeaa3464ba29ae3ad463de
Kbigstar <127897@naver.com>
2024-02-06 17:49:53 +0900

    day1

--- python_base/lotto_sample.py
+++ python_base/lotto_sample.py
@@ -1,3 +1,4 @@
+import random
 # 사용자가 로또 번호에 포함시키고 싶은 번호가 있다면
 # 1 ~ 5 개 까지만 입력을 받아서 사용자 번호가
 # 포함된 로또번호를 생성 해주세요
@@ -6,3 +7,8 @@ print(nums)
 set_nums = set(nums)
 print(set_nums)
 
+while True:
+    if len(set_nums) == 6:
+        print(set_nums)
+        break
+    set_nums.add(random.randint(1, 45))
