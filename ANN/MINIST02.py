19834c2c5326ab3de1f7a3e328c5cb8477b76c96
Kbigstar <127897@naver.com>
2024-02-28 17:35:21 +0900

    240228 ANN

---
+++ ANN/MINIST02.py
@@ -0,0 +1,45 @@
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+import  matplotlib.pyplot as plt
+import  tensorflow as tf
+#  매번 실행할때 랜덤값이 고정될 수 있도록
+np.random.seed(0)
+tf.random.set_seed(0)
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(f"학습데이터 : {x_train.shape}")
+print(f"테스트데이터 : {x_test.shape}")
+
+#  데이터 준비
+x_train_reshape = x_train.reshape(
+    x_train.shape[0], 784).astype("float32")/255
+x_test_reshape = x_test.reshape(
+    x_test.shape[0], 784).astype("float32")/255
+
+y_train_cate = to_categorical(y_train, 10)
+y_test_cate = to_categorical(y_test, 10)
+
+# model
+model = Sequential()
+model.add(Dense(512, input_dim=784, activation='relu'))
+
+# softmax는 다중 클래스 분류에서 사용되는 활성화 함수
+# 벡터의 원소를 0 ~ 1 사이값으로 변환
+# 변환된 값들의 합이 1이 되도록
+# 벡터를 확률 분포로 변환하는 역할을 함
+model.add(Dense(10, activation='softmax'))
+model.summary()
+model.compile(loss='categorical_crossentropy'
+              ,optimizer='adam', metrics=['acc'])
+
+history = model.fit(x_train_reshape
+                    ,y_train_cate
+                    ,batch_size=200
+                    ,validation_data=(x_test_reshape, y_test_cate))
+v_loss = history.history['val_loss']
+loss = history.history['loss']
+print(f'학습 acc : {model.evaluate(x_train_reshape, y_train_cate)}')
+print(f'테스트 acc : {model.evaluate(x_test_reshape, y_test_cate)}')
---
+++ ANN/perceptron01.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+# 퍼셉트론의 활성함수 step function
+def step_function(sum):
+    if sum > 0:
+        return 1
+    return 0
+
+class Perceptron:
+    def __init__(self, input_size):
+        self.w = np.zeros(input_size + 1) # +1bias
+    def predict(self, inputs):
+        sum = np.dot(inputs, self.w[1:]) + self.w[0]
+        return step_function(sum)
+    def train(self, train_inputs, labels, lr=0.01, epochs=100):
+        for i in range(epochs):
+            for input, label in zip(train_inputs, labels):
+                pred = self.predict(input)
+                self.w[1:] += lr * (label - pred) * input
+                self.w[0] += lr * (label - pred)
+# or 연산 데이터
+x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+# y = np.array([0, 1, 1, 1]) # OR 연산 정답
+# y = np.array([0, 0, 0, 1]) # AND 연산 정답
+y = np.array([0, 1, 1, 0]) # XOR 연산 정답 (출력값이 틀림)
+# (단순 Perceptron 에선 해결 불가)
+model = Perceptron(2)
+model.train(x, y, lr=0.1)
+for i, v in zip(x, y):
+    pred = model.predict(i)
+    print(f"입력 : {i}, 예측 : {pred}, 실제 : {v}")
+
+# 신경망
+from sklearn.neural_network import MLPClassifier
+m_model = MLPClassifier(hidden_layer_sizes=(4,), activation='relu'
+                      ,max_iter=10000, random_state=1)
+m_model.fit(x, y)
+print(m_model.predict(x)) # hidden layer 추가 후 XOR 연산 해결 가능
+
